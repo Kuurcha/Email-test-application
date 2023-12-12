8d55257473bcb71f29750bf2e8ad7dfd4cb20eeb
Kurcha <theyhyrik@gmail.com>
2023-12-12 16:26:23 +0300

    first commit

---
+++ Backend/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Test App is running...");
+// Keep the application running indefinitely
+setInterval(() => {
+    console.log("Test message at: ", new Date());
+}, 1000); // Log a message every second
