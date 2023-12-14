be130dcc4dda613b4bb72b7b9a95b74bd7e91f55
Kurcha <theyhyrik@gmail.com>
2023-12-14 08:19:05 +0300

    number mask added

--- Frontend/email-app/src/components/SearchForm/SearchForm.tsx
+++ Frontend/email-app/src/components/SearchForm/SearchForm.tsx
@@ -3,11 +3,34 @@ import { Button, Form } from "react-bootstrap";
 import "./SearchForm.css";
 const SearchForm = () => {
   const [email, setEmail] = useState("");
-  const [number, setNumber] = useState<number | undefined>(undefined);
+  const [number, setNumber] = useState<string | undefined>(undefined);
+  const removeNonDigitCharacters = (input: string): string => {
+    return input.replace(/\D/g, "");
+  };
 
-  const handleSubmit = async () => {};
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (number) {
+      const unformattedValue = removeNonDigitCharacters(number);
+      console.log("Submitted value:", unformattedValue);
+    }
+  };
+
+  const formatNumber = (input: string): string => {
+    const cleanedInput = removeNonDigitCharacters(input);
+    const formattedInput = cleanedInput.match(/.{1,2}/g)?.join("-") || "";
+    return formattedInput;
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e && e.target) {
+      const inputValue = e.target.value;
+      const formattedValue: string = formatNumber(inputValue);
+      setNumber(formattedValue);
+    }
+  };
   return (
-    <Form className="d-flex flex-column align-items-center form-container border p-4 m-auto">
+    <Form onSubmit={handleSubmit} className="d-flex flex-column align-items-center form-container border p-4 m-auto">
       <Form.Group controlId="formEmail" className="w-100">
         <Form.Label>Email (required)</Form.Label>
         <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
@@ -15,10 +38,10 @@ const SearchForm = () => {
 
       <Form.Group controlId="formNumber" className="w-100">
         <Form.Label>Number (optional)</Form.Label>
-        <Form.Control type="number" placeholder="Enter number" value={number || ""} onChange={(e) => setNumber(Number(e.target.value))} />
+        <Form.Control type="text" placeholder="Enter number" value={number || ""} onChange={handleInputChange} />
       </Form.Group>
 
-      <Button variant="secondary" className="mt-2" onClick={handleSubmit}>
+      <Button variant="secondary" className="mt-2" type="submit">
         Submit
       </Button>
     </Form>
